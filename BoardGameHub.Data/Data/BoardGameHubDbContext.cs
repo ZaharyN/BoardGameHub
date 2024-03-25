0f8457d574a2293a85741660cf60d65c3b826ed3
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-03-25 22:42:55 +0200

    Added Data layer

---
+++ BoardGameHub.Data/Data/BoardGameHubDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameHub.Data.Data
+{
+    public class BoardGameHubDbContext : IdentityDbContext
+    {
+        public BoardGameHubDbContext(DbContextOptions<BoardGameHubDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
