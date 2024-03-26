64fd2c190e2d8faa309073002aa7e7ab8de21fa2
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-03-26 21:50:05 +0200

    Boardgame entity made with its constants

---
+++ BoardGameHub.Data/Constants/DataConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameHub.Data.Constants
+{
+    public static class DataConstants
+    {
+        public const int BoardgameNameMaxLength = 200;
+        public const int BoardgameNameMinLength = 1;
+
+        public const int BoardGameDescriptionMaxLength = 1500;
+        public const int BoardGameDescriptionMinLength = 50;
+
+        public const string BoardGamePriceInShopMaxValue = "1000";
+        public const string BoardGamePriceInShopMinValue = "0";
+
+        public const int BoardGameMinimumPlayersMinValue = 2;
+        public const int BoardGameMinimumPlayersMaxValue = 4;
+
+        public const int BoardGameMaximumPlayersMinValue = 2;
+        public const int BoardGameMaximumPlayersMaxValue = 20;
+    }
+}
--- BoardGameHub.Data/Data/DataModels/Boardgame.cs
+++ BoardGameHub.Data/Data/DataModels/Boardgame.cs
@@ -1,13 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using static BoardGameHub.Data.Constants.DataConstants;
 
 namespace BoardGameHub.Data.Data.DataModels
 {
     public class Boardgame
     {
+        [Key]
+        public int Id { get; set; }
 
+        [Required]
+        [MaxLength(BoardgameNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [ForeignKey(nameof(Genre))]
+        public int GenreId { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; } = null!;
+
+        [Range(0, 5)]
+        public int? Rating { get; set; }
+
+        [Required]
+        [Display(Name = "Average playing time")]
+        public int AveragePlayingTime { get; set; }
+
+        [Required]
+        [MaxLength(BoardGameDescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.00, 5.00)]
+        public double Difficulty { get; set; }
+
+        [Required]
+        public DateTime YearPublished { get; set; }
+
+        [Required]
+        [Range(typeof(decimal),
+            BoardGamePriceInShopMinValue, 
+            BoardGamePriceInShopMaxValue, 
+            ErrorMessage = "Price must be between 0.00 and 1000.00 lv")]
+        public decimal PriceInShop { get; set; }
+
+        [Required]
+        [Range(BoardGameMinimumPlayersMinValue, BoardGameMinimumPlayersMaxValue)]
+        public int MinimumPlayersAllowedToPlay { get; set; }
+
+        [Required]
+        [Range(BoardGameMaximumPlayersMinValue, BoardGameMaximumPlayersMaxValue)]
+        public int MaximumPlayersAllowedToPlay { get; set; }
+
+        public List<GameReview>? Reviews { get; set; } = new List<GameReview>();
+
+        [Required]
+        public bool IsReserved { get; set; }
+
+        public Reservation? Reservation { get; set; }
+
+        [ForeignKey(nameof(Reservation))]
+        public int ReservationId { get; set; }
+        public bool IsUpcoming { get; set; }
     }
 }
