317c2e3b5635442dd0e471be39b416bec5a2678d
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-04-07 01:25:51 +0300

    Scaffolded Identity and edited Register page

--- BoardGameHub/Program.cs
+++ BoardGameHub/Program.cs
@@ -1,4 +1,7 @@
 using BoardGameHub.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BoardGameHub.Data.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
