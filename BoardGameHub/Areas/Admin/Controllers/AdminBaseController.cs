994f2fe5786699bbd4ebafcff33120639bea7f66
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-04-12 10:36:39 +0300

    Added controllers and methods to the admin area

--- BoardGameHub/Areas/Admin/Controllers/AdminBaseController.cs
+++ BoardGameHub/Areas/Admin/Controllers/AdminBaseController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BoardGameHub.Core.Contracts;
+using BoardGameHub.Core.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using static BoardGameHub.Data.Constants.DataConstants;
 
 namespace BoardGameHub.Areas.Admin.Controllers
---
+++ BoardGameHub/Areas/Admin/Controllers/BoardgameController.cs
@@ -0,0 +1,23 @@
+﻿using BoardGameHub.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardGameHub.Areas.Admin.Controllers
+{
+	public class BoardgameController : AdminBaseController
+	{
+		private readonly IBoardgameService boardgameService;
+
+		public BoardgameController(IBoardgameService _boardgameService)
+		{
+			boardgameService = _boardgameService;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ChangeGameStatus(int id)
+		{
+			await boardgameService.PromoteToActiveAsync(id);
+
+			return RedirectToAction("Active", "Boardgame");
+		}
+	}
+}
