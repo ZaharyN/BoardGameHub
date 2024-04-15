3fce4d5b0d951aefa3bebb640d5d7a5f57b50c45
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-04-16 00:08:09 +0300

    Fixed the routing to the create method

--- BoardGameHub/Areas/Admin/Controllers/BoardgameController.cs
+++ BoardGameHub/Areas/Admin/Controllers/BoardgameController.cs
@@ -1,22 +1,18 @@
 ﻿using BoardGameHub.Core.Contracts;
 using BoardGameHub.Core.Models.BoardgameViewModels;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Hosting;
 
 namespace BoardGameHub.Areas.Admin.Controllers
 {
 	public class BoardgameController : AdminBaseController
 	{
 		private readonly IBoardgameService boardgameService;
-		private readonly IWebHostEnvironment webHostEnvironment;
 
 		public BoardgameController(IBoardgameService _boardgameService,
 			 IWebHostEnvironment _webHostEnvironment)
 		{
 			boardgameService = _boardgameService;
-			webHostEnvironment = _webHostEnvironment;
 		}
 
 		[HttpPost]
@@ -25,11 +21,6 @@ namespace BoardGameHub.Areas.Admin.Controllers
 			await boardgameService.PromoteToActiveAsync(id);
 
 			return RedirectToAction("Active", "Boardgame", new { area = "" });
-		}
-
-		public async Task<IActionResult> ViewAll()
-		{
-			return View();
 		}
 
 		[HttpGet]
