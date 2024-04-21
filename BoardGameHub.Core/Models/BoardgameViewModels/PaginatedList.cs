5083fbb06eb8f01063c45bb0e5fc579c8d2cef1f
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2024-04-21 17:21:07 +0300

    Added paginated list model and implementation

---
+++ BoardGameHub.Core/Models/BoardgameViewModels/PaginatedList.cs
@@ -0,0 +1,32 @@
+﻿namespace BoardGameHub.Core.Models.BoardgameViewModels
+{
+	public class PaginatedList
+	{
+        public int TotalItems { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
+
+        public PaginatedList()
+        {
+            
+        }
+        public PaginatedList(int totalItems, int page, int pageSize = 8)
+        {
+            int totalPages = (int)Math.Ceiling((decimal)totalItems / pageSize);
+            int currentPage = page;
+
+            int startPage = 1;
+            int endPage = totalPages;
+
+            TotalItems = totalItems;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPages = totalPages;
+            StartPage = startPage;
+            EndPage = endPage;
+        }
+    }
+}
