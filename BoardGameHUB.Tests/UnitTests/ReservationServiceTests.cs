9a07397c5ebcb9f8f71c42cc505f1488f289f2e0
ZaharyN <zlnyagolov@abv.bg>
2024-04-23 12:34:08 +0300

    Added ReservationServiceTests folder

---
+++ BoardGameHUB.Tests/UnitTests/ReservationServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using BoardGameHub.Core.Contracts;
+using BoardGameHub.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameHUB.Tests.UnitTests
+{
+    [TestFixture]
+    public class ReservationServiceTests : UnitTestsBase
+    {
+        private IReservationService reservationService;
+
+        [OneTimeSetUp]
+        public void SetUp() => reservationService = new ReservationService(context);
+
+    }
+}
