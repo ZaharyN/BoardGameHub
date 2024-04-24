646f37cd3e02a8579996668cc0c32ec8d12f42eb
ZaharyN <zlnyagolov@abv.bg>
2024-04-24 11:58:37 +0300

    ApplicationUserService tested

---
+++ BoardGameHUB.Tests/UnitTests/IApplicationUserServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using BoardGameHub.Core.Contracts;
+using BoardGameHub.Core.Services;
+
+namespace BoardGameHUB.Tests.UnitTests
+{
+    [TestFixture]
+    public class IApplicationUserServiceTests : UnitTestsBase
+    {
+        private IApplicationUserService applicationUserService;
+
+        [OneTimeSetUp]
+        public void SetUp() => applicationUserService = new ApplicationUserService(context);
+
+        [Test]
+        public async Task UserFullNameShouldWorkProperly()
+        {
+            string validId = AppUser1.Id;
+
+            string fullName = await applicationUserService.UserFullName(validId);
+
+            Assert.AreEqual(fullName, $"{AppUser1.FirstName} {AppUser1.LastName}");
+        }
+
+        [Test]
+        public async Task UserFullNameShouldReturnNullIfUserHasNoName()
+        {
+            AppUser1.FirstName = string.Empty;
+
+            var result1 = await applicationUserService.UserFullName(AppUser1.Id);
+
+            Assert.IsNull(result1);
+
+            AppUser1.FirstName = "Ivan";
+            AppUser1.LastName = null;
+            var result2 = await applicationUserService.UserFullName(AppUser1.Id);
+
+            Assert.IsNull(result2);
+        }
+    }
+}
